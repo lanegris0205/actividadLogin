e5b2968e557c2557079c738152d595ed6d79a7d7
leydi <leyramirez@misena.edu.co>
2021-08-07 16:55:08 -0500

    modificacion archivos

---
+++ pagUsuario.php
@@ -0,0 +1,46 @@
+<?php
+// session_start();
+require_once ('Database.php');
+class Usuario
+{
+    protected $database;
+    private $id;
+    public $email;
+    public $password;
+
+    public function __construct()
+    {
+        session_start();
+       
+    }
+
+    public function validar($email,$password)
+    {
+        $newDatabase = new Database();
+        $database = $newDatabase->init();
+        $query = $database->prepare("SELECT * FROM usuariologin
+        WHERE email = :email AND password = :password");
+        $query->bindParam(":email", $email);
+        $query->bindParam(":password", $password);
+        $query->execute();
+        $usuario = $query->fetch(PDO::FETCH_ASSOC);
+        //echo($usuario['']);
+        if ($usuario) {
+            $_SESSION['email'] = $usuario["email"];
+            header("location:http://localhost\ActividadLogin\pagInicio.php");
+        } else {
+            $msg = "Email o contraseña no validos";
+            $aPahtOrigin = explode('?', $_SERVER['HTTP_REFERER']);
+            $pahtOrigin = $aPahtOrigin[0];
+            //var_dump($aPahtOrigin);
+            header("Location: $pahtOrigin?msg=$msg");
+        }
+    }
+
+    function cerrarsesion()
+    {
+        session_unset();
+        session_destroy();
+        header("location:http://localhost\ActividadLogin\login.php");
+    }
+}
---
+++ validacion.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once ('pagUsuario.php');
+
+$usuario=new Usuario();
+$email = $_REQUEST['email'];
+$password = $_REQUEST['password'];
+
+$usuario->validar($email,$password);
+if($_REQUEST['cerrar']){
+    $usuario->cerrarsesion();
+}
+
+
+
+?>
